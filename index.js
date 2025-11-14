91df334c1e7ff0479b4f5af095e1771dc492e185
Martin Saposnic <martinsaposnic@gmail.com>
2025-11-14 15:52:40 -0300

    add pay to lnurl capability, also add other methods that may be helpful

--- index.js
+++ index.js
@@ -284,8 +284,8 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { generateMnemonic, setLogListener, MdkNode } = nativeBinding
+const { setLogListener, generateMnemonic, MdkNode } = nativeBinding
 
-module.exports.generateMnemonic = generateMnemonic
 module.exports.setLogListener = setLogListener
+module.exports.generateMnemonic = generateMnemonic
 module.exports.MdkNode = MdkNode
